f5cbb16ce5c2d0084a87ea53db9489f702d66e0a
Zero AP <ys.ahnpark@hanaloop.com>
2022-11-01 17:30:17 +0900

    Add initial assignment code

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { Button } from "ui";
 
 export default function Web() {
+
+  const [serverInfo, setServerInfo] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:9080/climatix/info');
+      setServerInfo(await response.json());
+
+      // fetch('http://localhost:9080/climatix/info')
+      // .then(response => setServerInfo(response.json()));
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <pre>{
+        JSON.stringify(serverInfo, null, 2)
+      }</pre>
     </div>
   );
 }
